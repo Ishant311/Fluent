ce5462d051a65ce43f4b7d81d9d6da9f27ffe570
Ishant Arora <ishantarora311@gmail.com>
2025-08-31 15:13:42 +0530

    Initial version of communication project

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    return res.send("Connection ho gya bhai!!!");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
